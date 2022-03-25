58f6b28c2833e99871e527b3f23f52eeffd87119
Ricardo Gamarra <r.richard0000@gmail.com>
2022-03-25 09:25:10 -0300

    Render images in the movie card component

--- app/javascript/components/MovieCard.jsx
+++ app/javascript/components/MovieCard.jsx
@@ -1,8 +1,32 @@
-import { Card } from "antd";
+import { Card, Skeleton } from "antd";
+import _ from "lodash";
 import React from "react";
+import { imageUrl } from "../services/api";
+import moment from "moment";
 
 const MovieCard = ({ movie }) => {
-  return <Card title={movie.title}>Card content</Card>;
+  return (
+    <Card
+      style={{ width: 150 }}
+      cover={
+        <img
+          alt={`Movie poster for ${movie.title}`}
+          src={imageUrl(movie.posterPath)}
+        />
+      }
+    >
+      <MovieCardFooter movie={movie} />
+    </Card>
+  );
+};
+
+const MovieCardFooter = ({ movie }) => {
+  return (
+    <>
+      <p>{movie.title}</p>
+      <p>{moment(movie.releaseDate).format("MMM D, YYYY")}</p>
+    </>
+  );
 };
 
 export default MovieCard;
--- app/javascript/services/api.jsx
+++ app/javascript/services/api.jsx
@@ -1,9 +1,16 @@
 import _ from "lodash";
 import xhr from "lib/xhr";
 
+export const imageUrl = (imageSlug) =>
+  `https://image.tmdb.org/t/p/w500/${imageSlug}`;
 export const apiUrl = (path) => `/api/v1/${path}`;
 
 export const fetchMovies = async () => {
   const url = apiUrl(`movies`);
+  return await xhr.get(url);
+};
+
+export const fetchMoviePoster = async (id) => {
+  const url = apiUrl(`movies/${id}/poster`);
   return await xhr.get(url);
 };
